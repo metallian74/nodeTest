dbeb5f1606122ea14cf71728112b04e9cc2efabc
shide <hideux.sebastien@gmail.com>
2015-08-03 10:03:43 +0200

    test

--- example.js
+++ example.js
@@ -5,7 +5,7 @@ http.createServer(function(req,res) {
 }).listen(1337, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:1337/');
 */
-
+/*
 var net = require('net');
 
 var server = net.createServer(function (socket) {
@@ -13,4 +13,53 @@ var server = net.createServer(function (socket) {
   socket.pipe(socket);
 });
 
-server.listen(1337, '127.0.0.1');
+server.listen(1337, '127.0.0.1');*/
+/*
+var res = 0;
+for (var i = process.argv.length - 1; i >= 0; i--) {
+	var val = process.argv[i];
+	if (!!parseInt(val)) {
+		res += parseInt(val);
+	};
+//	console.argv[i]
+
+};
+console.log(res);
+*/
+/*
+console.log(process.argv);*/
+/*var fs = require('fs');
+/*
+for (var i = process.argv.length - 1; i >= 0; i--) {*/
+/*	var buffer  = fs.readFileSync(process.argv[2])
+	var str = buffer.toString();
+	console.log(str.split('\n').length - 1);*/
+/*};
+*/
+
+/*
+var fs = require('fs');
+
+fs.readFile(process.argv[2], function (err, data) {
+  if (err) 
+  	throw err;
+  
+  console.log(data.toString().split('\n').length - 1);
+
+});*/
+
+
+/*console.log(process.argv);*/
+var fs = require('fs');
+fs.readdir(process.argv[2], function(err, list){
+	if (err) {throw err};
+	var filter = process.argv[3];
+
+	for (var i = 0; i < list.length; i++) {
+		if (list[i].indexOf('.'+ filter)>0) {
+			console.log(list[i]);
+
+		};
+	};
+
+})
